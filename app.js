91424765cd35b5a388e07d7ccd6f5356f9787710
Sadik Hussain <sadikhussainpsb2@gmail.com>
2022-07-20 19:28:50 +0530

    rechanges

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(
-  "mongodb+srv://sadik123:Sad2020@cluster0.ybsqgyi.mongodb.net/blogDB",
+  "mongodb+srv://sadik123:Sad2020@cluster0.ybsqgyi.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
   }
